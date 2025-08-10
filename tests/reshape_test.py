87bb9b23fd8df47e080a57221ff726ea26a0d353
Hao Zhang <hzhangxyz@outlook.com>
2025-08-11 02:36:42 +0800

    Add more tests for reshape function.

--- tests/reshape_test.py
+++ tests/reshape_test.py
@@ -15,3 +15,21 @@ def test_reshape_consistency(arrow: tuple[bool, ...], plan_range: tuple[int, int
     b = a.reshape(plan)
     c = b.reshape(a.edges)
     assert torch.allclose(a.tensor, c.tensor)
+
+
+ReshapeWithoutArrow = tuple[tuple[tuple[int, int], ...]]
+
+
+@pytest.mark.parametrize("x", [
+    (((1, 2), (3, 4)), ),
+    (((5, 6), (7, 8)), ),
+    (((1, 2), (2, 1), (2, 2)), ),
+])
+def test_reshape_without_arrow(x: ReshapeWithoutArrow) -> None:
+    edges, = x
+    shape = tuple(even + odd for (even, odd) in edges)
+    tensor = GrassmannTensor(tuple(False for _ in edges), edges, torch.randn(shape)).update_mask()
+    merged = tensor.reshape([tensor.tensor.numel()])
+    assert torch.allclose(tensor.tensor.reshape([-1]), merged.tensor)
+    split = merged.reshape(edges)
+    assert torch.allclose(tensor.tensor, split.tensor)
